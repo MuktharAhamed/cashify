89132e3cabf39e37f1e4fc255e34d09b07e7d3f6
Mukthar Ahmed <mamubeen6@gmail.com>
2021-07-24 17:10:07 +0530

    product detail spec

--- src/views/ProductDetail/Specifications.js
+++ src/views/ProductDetail/Specifications.js
@@ -1,43 +1,27 @@
 import styles from './style';
-import Icon from 'react-native-vector-icons/MaterialIcons';
 import React, {useState, useEffect} from 'react';
-import {
-  FlatList,
-  Image,
-  SafeAreaView,
-  Dimensions,
-  ScrollView,
-  View,
-  TouchableOpacity,
-  LayoutAnimation,
-  Text,
-  TextInput,
-  UIManager,
-} from 'react-native';
+import {View, Text} from 'react-native';
+
 const Specifications = props => {
-  //   useEffect(() => {
-  //     console.log('props');
-  //     console.log(props);
-  //     var abs = props.availableOptions.sort((a, b) => {
-  //       return a.value - b.value;
-  //     });
-  //     console.log('abs');
-  //     console.log(abs);
-  //   }, []);
+  console.log('Specifications', props.Specifications);
   return (
-    <FlatList
-      data={props.Specifications}
-      keyExtractor={item => item.node.id}
-      renderItem={({item, index}) => <SpecificationBlock item={item} />}
-      horizontal={false}
-      numColumns={2}
-      //   scrollEnabled={false}
-    />
+    <View style={styles.specificationmainview}>
+      {props?.Specifications.map(e => (
+        <SpecificationBlock item={e} />
+      ))}
+    </View>
   );
 };
 
 const SpecificationBlock = ({item}) => {
-  return <View></View>;
+  console.log('Specificationsitem', item);
+  return (
+    <View style={styles.specificationview}>
+      <Text>
+        {item.node.key}:{item.node.value}
+      </Text>
+    </View>
+  );
 };
 
 export default Specifications;
